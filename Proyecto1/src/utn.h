12bc0b2d936722ec16a9ce3ea5aaf579affd5c6d
Martinsauvage7 <martin93cuq@gmail.com>
2022-04-19 13:49:02 -0300

    Entrega del trabajo practico

---
+++ Proyecto1/src/utn.h
@@ -0,0 +1,7 @@
+#ifndef UTN_H_
+#define UTN_H_
+
+int utn_getInt(int* pResultado, char * mensaje, char* mensajeError, int minimo, int maximo, int reintentos);
+int utn_getFloat(float* pResultado, char * mensaje, char* mensajeError, float minimo, float maximo, int reintentos);
+int utn_getChar(char* pResultado, char * mensaje, char* mensajeError, char minimo, char maximo, int reintentos);
+#endif /* UTN_H_ */
